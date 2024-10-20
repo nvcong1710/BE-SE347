c99086776beae028cdabcd308512d8f98dea17dd
Nguyễn Viết Công <91941271+nvcong1710@users.noreply.github.com>
2024-10-20 12:34:56 +0700

    update authenticate & jwt

---
+++ src/main/java/com/uit/website_ban_sach/entity/TaiKhoan.java
@@ -0,0 +1,29 @@
+package com.uit.website_ban_sach.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "TaiKhoan")
+public class TaiKhoan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER,
+            cascade = {CascadeType.PERSIST,
+                    CascadeType.MERGE, CascadeType.DETACH})
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Collection<Role> roles = new HashSet<>();
+    private String email;
+    private String gioiTinh;
+}
