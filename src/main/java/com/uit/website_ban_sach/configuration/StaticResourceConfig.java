d26eec75329f36c6dbdddd737db12a3fea556dd6
ThienDinh203 <toidaykt357@gmail.com>
2024-12-28 11:34:33 +0700

    update donhang dto

--- src/main/java/com/uit/website_ban_sach/configuration/StaticResourceConfig.java
+++ src/main/java/com/uit/website_ban_sach/configuration/StaticResourceConfig.java
@@ -1,6 +1,5 @@
 package com.uit.website_ban_sach.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
